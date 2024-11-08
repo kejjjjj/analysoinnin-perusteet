b60938654d0036e8bd8678df7c1dd56e108f31e9
kejjjjj <taas.xd@gmail.com>
2024-11-08 14:11:28 +0200

    printer

---
+++ harjoitukset/printer.py
@@ -0,0 +1,16 @@
+
+title = "This app is made by: "
+creator = "testi"
+var1 = 1
+var2 = 2
+
+# print(var1)
+
+print("the value of var1 is ", var1)
+print("the value of var2 is ", var2)
+
+multiplication = var1 * var2
+print(var1, " * ", var2, " = ", var1 * var2)
+print(var1, " + ", var2, " = ", var1 + var2)
+print(var1, " - ", var2, " = ", var1 - var2)
+print(var1, " / ", var2, " = ", var1 / var2)
